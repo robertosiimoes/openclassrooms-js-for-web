69df00ab6926f77bcbd590a2c088dbabe72330bf
Roberto SIMOES <robertosiimoes@gmail.com>
2020-10-26 20:39:47 +0100

    Working on Ajax sending data, Content-type and others readyStatus values

--- js/ajax.js
+++ js/ajax.js
@@ -1,53 +1,116 @@
 // Script JS
 
-	// Storing button into variable
-	// var btnAskWeather = document.getElementById('ask-weather');
 
-	// Listening click event on that button
-	// btnAskWeather.addEventListener('click', function(e) {
 
-	// 	// Preventing the default behaviour
-	// 	e.preventDefault();
-		
-	// 	// Creating the request
-	// 	var xhr = new XMLHttpRequest();
-	// 	var weatherResult = document.getElementById('weather-result');
-	
-	// 	// Checking the status changing
-	// 	xhr.onreadystatechange = function() {
+	/* Exercice on how we can fetch Weather with Ajax
+
+		// Storing button into variable
+		var btnAskWeather = document.getElementById('ask-weather');
+
+		// Listening click event on that button
+		btnAskWeather.addEventListener('click', function(e) {
+
+			// Preventing the default behaviour
+			e.preventDefault();
 			
-	// 		if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-	// 			var response = JSON.parse(xhr.responseText);
-	// 			weatherResult.innerHTML = response.current_condition.condition;
-	// 		} else {
-	// 			console.log("There is an error !");
-	// 		}
-	// 	}
-	
-	// 	// Fetching the Weather API
-	// 	xhr.open('GET', 'https://www.prevision-meteo.ch/services/json/paris', true);
-	// 	xhr.send();
-	// });
+			// Creating the request
+			var xhr = new XMLHttpRequest();
+			var weatherResult = document.getElementById('weather-result');
+		
+			// Checking the status changing
+			xhr.onreadystatechange = function() {
+				
+				if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+					var response = JSON.parse(xhr.responseText);
+					weatherResult.innerHTML = response.current_condition.condition;
+				} else {
+					console.log("There is an error !");
+				}
+			}
+		
+			// Fetching the Weather API
+			xhr.open('GET', 'https://www.prevision-meteo.ch/services/json/paris', true);
+			xhr.send();
+		});
 
-
-	/*
-	
-	Nous souhaitons dans un premier temps valider le champ Code du formulaire.
-	A chaque lettre saisie dans le champ ayant pour ID code nous voulons vérifier que la valeur du champ commence bien par CODE- grâce à une Regex que voici : /^CODE-/.
-	Si la valeur commence bien par CODE- alors nous affichons dans l'élément ayant pour ID code-validation : Code valide, sinon nous affichons dans cet élément Code invalide.
-
-	Maintenant que nous savons si notre code est valide ou non, nous voudrions griser (grâce à l'attribut disabled) le bouton de soumission (L'input de type submit ayant pour ID submit-btn) quand le code est invalide, et le dégriser quand le code est valide.
-
-	Cela signifie que nous allons devoir ajouter un attribut disabled="true" au bouton de soumission quand le code est invalide. Et supprimer cet attribut disabled quand le code est valide (Rappelez-vous du cours sur la modification du DOM pour définir et supprimer des attributs).
-
-	Finalement, nous avons un champ Email et nous voudrions le rendre obligatoire et obliger l'utilisateur à entrer une adresse email valide. Il faudra aussi empêcher le formulaire de se soumettre s'il n'est pas valide.
-
-	Mais nous voudrions faire tout ça uniquement avec le HTML5, sans utiliser de code JavaScript.
-
-	Vous pouvez changer le type du champ email plutôt que d'utiliser une Regex via pattern
-	
-	
 	*/
 
-	// Code here
-	
+
+
+	/* Exercice on how to verify user data forms
+
+		// Code here
+		var inputCode = document.getElementById("code");
+		var btnSubmit = document.getElementById("submit-btn");
+
+		// Test value of input and return boolean
+		function testValue (value) {
+			return /^CODE-/.test(value);
+		}
+
+		inputCode.addEventListener("input", function(e) {
+
+			e.preventDefault();
+
+			var input = e.target.value;
+			var codeResult = document.getElementById("code-validation");
+
+			// Test if value is in correct format
+			if ( testValue(input) == true ) {
+				codeResult.textContent = "Code valide.";
+				btnSubmit.setAttribute("disabled", "");
+			} else {
+				codeResult.textContent = "Code invalide.";
+				btnSubmit.removeAttribute("disabled", "");
+			}
+		});
+
+	*/
+
+
+	/* Exercice on how to save data on web services */
+
+	var btnSubmit = document.querySelector("input[type='submit']");
+
+	function sendRequest() {
+
+		xhr = new XMLHttpRequest();
+
+		xhr.onreadystatechange = function () {
+
+			// console.log(this.readyState);
+			
+			if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+				var response = JSON.parse(this.responseText);
+				var result = document.getElementById("result");
+				result.textContent = response.postData.text;
+
+				console.log("Operation is finished.");
+			}
+
+			else if (this.readyState == XMLHttpRequest.LOADING) {
+				console.log("Upload; responseText contains partials data.");
+			}
+
+			else if (this.readyState == XMLHttpRequest.HEADERS_RECEIVED) {
+				console.log("The method send was call. Headers and status are availiable.");
+			}
+
+			else if (this.readyState == XMLHttpRequest.OPENED) {
+				console.log("The method open was call.");
+			}
+
+			else {
+				console.log("The client was created. But open method was not yet call.");
+			}
+		}
+
+		xhr.open("POST", "https://mockbin.com/request", true);
+		xhr.setRequestHeader("Content-type", "application/json");
+		xhr.send(JSON.stringify({value: document.getElementById("value").value}));
+	}
+
+	btnSubmit.addEventListener('click', function(e) {
+		e.preventDefault();
+		sendRequest();
+	});
