6b63324d067add6d5339f32f78107ebd461260fc
Roberto SIMOES <robertosiimoes@gmail.com>
2020-10-27 02:38:55 +0100

    Working on callbacks, promises and async/await concepts

--- js/ajax.js
+++ js/ajax.js
@@ -2,7 +2,7 @@
 
 
 
-	/* Exercice on how we can fetch Weather with Ajax
+	/* Exercice 01 : How we can fetch weather data with Ajax
 
 		// Storing button into variable
 		var btnAskWeather = document.getElementById('ask-weather');
@@ -37,7 +37,7 @@
 
 
 
-	/* Exercice on how to verify user data forms
+	/* Exercice 02 : How to verify user data forms
 
 		// Code here
 		var inputCode = document.getElementById("code");
@@ -68,49 +68,83 @@
 	*/
 
 
-	/* Exercice on how to save data on web services */
+	/* Exercice 03 : How to save data on web services
 
-	var btnSubmit = document.querySelector("input[type='submit']");
+		var btnSubmit = document.querySelector("input[type='submit']");
 
-	function sendRequest() {
+		function sendRequest() {
 
-		xhr = new XMLHttpRequest();
+			xhr = new XMLHttpRequest();
 
-		xhr.onreadystatechange = function () {
+			xhr.onreadystatechange = function () {
 
-			// console.log(this.readyState);
-			
-			if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-				var response = JSON.parse(this.responseText);
-				var result = document.getElementById("result");
-				result.textContent = response.postData.text;
+				// console.log(this.readyState);
+				
+				if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+					var response = JSON.parse(this.responseText);
+					var result = document.getElementById("result");
+					result.textContent = response.postData.text;
 
-				console.log("Operation is finished.");
+					console.log("Operation is finished.");
+				}
+
+				else if (this.readyState == XMLHttpRequest.LOADING) {
+					console.log("Upload; responseText contains partials data.");
+				}
+
+				else if (this.readyState == XMLHttpRequest.HEADERS_RECEIVED) {
+					console.log("The method send was call. Headers and status are availiable.");
+				}
+
+				else if (this.readyState == XMLHttpRequest.OPENED) {
+					console.log("The method open was call.");
+				}
+
+				else {
+					console.log("The client was created. But open method was not yet call.");
+				}
 			}
 
-			else if (this.readyState == XMLHttpRequest.LOADING) {
-				console.log("Upload; responseText contains partials data.");
-			}
-
-			else if (this.readyState == XMLHttpRequest.HEADERS_RECEIVED) {
-				console.log("The method send was call. Headers and status are availiable.");
-			}
-
-			else if (this.readyState == XMLHttpRequest.OPENED) {
-				console.log("The method open was call.");
-			}
-
-			else {
-				console.log("The client was created. But open method was not yet call.");
-			}
+			xhr.open("POST", "https://mockbin.com/request", true);
+			xhr.setRequestHeader("Content-type", "application/json");
+			xhr.send(JSON.stringify({value: document.getElementById("value").value}));
 		}
 
-		xhr.open("POST", "https://mockbin.com/request", true);
-		xhr.setRequestHeader("Content-type", "application/json");
-		xhr.send(JSON.stringify({value: document.getElementById("value").value}));
-	}
+		btnSubmit.addEventListener('click', function(e) {
+			e.preventDefault();
+			sendRequest();
+		});
 
-	btnSubmit.addEventListener('click', function(e) {
-		e.preventDefault();
-		sendRequest();
-	});
+	*/
+
+
+	/* Exercice 04 : How we can use Callbacks, Promises and Async_Await
+		
+		async function getNumber1() {
+			return 10;
+		}
+
+		async function getNumber2() {
+			return 4;
+		}
+
+		async function compute() {
+			var numberOne = await getNumber1();
+			var numberTwo = await getNumber2();
+		
+			return numberOne + numberTwo;
+		}
+
+		var result = document.getElementById("result");
+
+		compute()
+			.then(function(data) {
+				console.log("Everything OK", data);
+				result.textContent = data;
+			})
+			.catch(function(data) {
+				console.log("Error", data);
+				result.textContent = data;
+			})
+			
+	*/
